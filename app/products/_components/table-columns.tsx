757bf2203052b336b932d0bb02f8eee03c0ffbe4
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-01-17 15:50:04 -0300

    feat: install badge column status and config 'in' or 'out' of the stock, with coditions style colors

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -1,8 +1,16 @@
-"use client"
+"use client";
 
-import { Product } from "@prisma/client"
-import { ColumnDef } from "@tanstack/react-table"
+import { Badge } from "@/app/_components/ui/badge";
+import { Product } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { CircleIcon } from "lucide-react";
 
+const getStatusLabel = (status: string) => {
+  if (status === "IN_STOCK") {
+    return "Em estoque";
+  }
+  return "Fora de estoque";
+};
 export const productTableColumns: ColumnDef<Product>[] = [
   {
     accessorKey: "name",
@@ -10,7 +18,7 @@ export const productTableColumns: ColumnDef<Product>[] = [
   },
   {
     accessorKey: "price",
-    header: "Valor Unitário",
+    header: "Valor unitário",
   },
   {
     accessorKey: "stock",
@@ -19,5 +27,14 @@ export const productTableColumns: ColumnDef<Product>[] = [
   {
     accessorKey: "status",
     header: "Status",
+    cell: (row) => {
+      const product = row.row.original;
+      const label = getStatusLabel(product.status);
+      return <Badge
+        variant={label === "Em estoque" ? "default" : "destructive"} className="gap-1.5" >
+        <CircleIcon size={14} className={`${label === "Em estoque" ? "fill-green-500" : "fill-red-500"}`} /> {label}
+      </Badge>
+        ;
+    },
   },
-]
+];
