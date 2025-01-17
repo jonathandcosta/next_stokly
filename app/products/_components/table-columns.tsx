3ab9696a69d29c05f8133657e71619c8302fab51
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-01-17 14:20:05 -0300

    feat: config table

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -1,27 +1,23 @@
 "use client"
 
+import { Product } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
-export type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
-}
-
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: "Produto",
+  },
+  {
+    accessorKey: "price",
+    header: "Valor Unitário",
+  },
+  {
+    accessorKey: "stock",
+    header: "Estoque",
+  },
   {
     accessorKey: "status",
     header: "Status",
-  },
-  {
-    accessorKey: "email",
-    header: "Email",
-  },
-  {
-    accessorKey: "amount",
-    header: "Amount",
   },
 ]
