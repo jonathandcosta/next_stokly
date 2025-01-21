c62cce357b9a1dfadc878815dda1e1d0af47405c
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-01-21 18:44:29 -0300

    rafctor: cont (passing data correctly)

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -19,7 +19,8 @@ const ProductsPage = async () => {
           Novo Produto
         </Button>
       </div>
-      <DataTable columns={productTableColumns} data={products} />
+      <DataTable columns={productTableColumns}
+        data={JSON.parse(JSON.stringify(products))} />
     </div>
   );
 }
