116f0c9a3ba58caa354c3c65a6395023883db92c
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-07-17 16:24:24 -0300

    feat: insert input's and products

--- app/sales/_components/upsert-sheet-content.tsx
+++ app/sales/_components/upsert-sheet-content.tsx
@@ -1,6 +1,36 @@
-import { SheetContent, SheetDescription, SheetTitle, SheetHeader } from "@/app/_components/ui/sheet";
+"use client";
 
-const upsertSheetContent = () => {
+import { Combobox, ComboboxOption } from "@/app/_components/ui/combobox";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/app/_components/ui/form";
+import { Input } from "@/app/_components/ui/input";
+import { SheetContent, SheetDescription, SheetTitle, SheetHeader } from "@/app/_components/ui/sheet";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  productId: z.string().uuid(),
+  quatity: z.number().int().positive(),
+})
+
+type FormSchema = z.infer<typeof formSchema>;
+
+interface UpsertSheetContentProps {
+  productsOptions: ComboboxOption[];
+}
+
+const UpsertSheetContent = ({ productsOptions }: UpsertSheetContentProps) => {
+
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      productId: "",
+      quatity: 1,
+    },
+  })
+
+
+
   return (
     <SheetContent>
       <SheetHeader>
@@ -9,8 +39,47 @@ const upsertSheetContent = () => {
           Preencha os campos abaixo para criar ou editar uma venda.
         </SheetDescription>
       </SheetHeader>
+
+      <Form {...form}>
+        <form className="space-y-4">
+          <FormField
+            control={form.control}
+            name="productId"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Produto</FormLabel>
+                <FormControl>
+                  <Combobox
+                    placeholder="Selecione um produto"
+                    options={productsOptions}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="quatity"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Quantidade</FormLabel>
+                <FormControl>
+                  <Input type="number" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Add submit button or other actions here */}
+        </form>
+
+      </Form>
     </SheetContent>
   );
-}
+};
 
-export default upsertSheetContent;
+export default UpsertSheetContent;
--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,8 +1,19 @@
 import { Button } from "../_components/ui/button";
+import { ComboboxOption } from "../_components/ui/combobox";
 import { Sheet, SheetTrigger } from "../_components/ui/sheet";
+import { getProducts } from "../_data-acess/product/get-produts";
 import UpsertSheetContent from "./_components/upsert-sheet-content";
 
-const Sales = () => {
+const Sales = async () => {
+  const products = await getProducts()
+
+  // Tratar os dados aqui no servidor
+  const productsOptions: ComboboxOption[] = products.map((product) => ({
+    value: product.id,
+    label: product.name,
+  }));
+
+
   return (
     <div className="w-full space-y-8 p-8 bg-white ml-8">
       <div className="flex w-full items-center justify-between">
@@ -12,11 +23,9 @@ const Sales = () => {
         </div>
         <Sheet>
           <SheetTrigger asChild>
-            <Button className="bg-green-500 hover:bg-green-600 text-white">
-              Criar Venda
-            </Button>
+            <Button>Criar Venda</Button>
           </SheetTrigger>
-          <UpsertSheetContent />
+          <UpsertSheetContent productsOptions={productsOptions} />
         </Sheet>
       </div>
       {/* <DataTable columns={productTableColumns} data={JSON.parse(JSON.stringify(products))} /> */}
