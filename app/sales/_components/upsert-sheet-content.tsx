533fa6dc00ed90443bcdae13015ef48b185f1439
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-07-21 19:13:56 -0300

    feat: select input product

--- app/sales/_components/upsert-sheet-content.tsx
+++ app/sales/_components/upsert-sheet-content.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import { Button } from "@/app/_components/ui/button";
 import { Combobox, ComboboxOption } from "@/app/_components/ui/combobox";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/app/_components/ui/form";
 import { Input } from "@/app/_components/ui/input";
 import { SheetContent, SheetDescription, SheetTitle, SheetHeader } from "@/app/_components/ui/sheet";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { PlusIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const formSchema = z.object({
-  productId: z.string().uuid(),
-  quatity: z.number().int().positive(),
+  productId: z.string().uuid({
+    message: "Selecione um produto válido",
+  }),
+  quatity: z.coerce.number().int().positive(),
 })
 
 type FormSchema = z.infer<typeof formSchema>;
@@ -29,6 +33,10 @@ const UpsertSheetContent = ({ productsOptions }: UpsertSheetContentProps) => {
     },
   })
 
+  const onSubmit = (data: FormSchema) => {
+    // Handle form submission logic here
+    console.log(data);
+  };
 
 
   return (
@@ -41,7 +49,7 @@ const UpsertSheetContent = ({ productsOptions }: UpsertSheetContentProps) => {
       </SheetHeader>
 
       <Form {...form}>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
           <FormField
             control={form.control}
             name="productId"
@@ -75,6 +83,10 @@ const UpsertSheetContent = ({ productsOptions }: UpsertSheetContentProps) => {
           />
 
           {/* Add submit button or other actions here */}
+          <Button type="submit" className="w-full gap-2" variant='secondary'>
+            <PlusIcon size={20} />
+            Adicionar produto à venda
+          </Button>
         </form>
 
       </Form>
